eb8cdbbc1eebb4d76fe2188d2e60234149936765
God <calvindemeester@gmail.com>
2018-01-08 15:52:07 +0100

    update
    update

---
+++ Escaperoom - Copy/Escaperoom/Groepen_koppelen.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Escaperoom
+{
+    partial class Groepen_koppelen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.deelnemerBox = new System.Windows.Forms.TextBox();
+            this.createButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.groepnaamBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(269, 135);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(127, 30);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Toevoegen";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // deelnemerBox
+            // 
+            this.deelnemerBox.Location = new System.Drawing.Point(269, 99);
+            this.deelnemerBox.Name = "deelnemerBox";
+            this.deelnemerBox.Size = new System.Drawing.Size(127, 22);
+            this.deelnemerBox.TabIndex = 1;
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(36, 287);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(201, 59);
+            this.createButton.TabIndex = 2;
+            this.createButton.Text = "Maak groep aan";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Deelnemer toevoegen:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(36, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 148);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Deelnemers:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Groepsnaam:";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(269, 171);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(127, 30);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Verwijderen";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groepnaamBox
+            // 
+            this.groepnaamBox.Location = new System.Drawing.Point(37, 37);
+            this.groepnaamBox.Name = "groepnaamBox";
+            this.groepnaamBox.Size = new System.Drawing.Size(200, 22);
+            this.groepnaamBox.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Kies bestaande";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Groepen_koppelen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 377);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groepnaamBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.deelnemerBox);
+            this.Controls.Add(this.addButton);
+            this.Name = "Groepen_koppelen";
+            this.Text = "Groepen maken";
+            this.Load += new System.EventHandler(this.Groepen_koppelen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox deelnemerBox;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox groepnaamBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Escaperoom - Copy/Escaperoom/Groeplijst.cs
@@ -0,0 +1,63 @@
+﻿using Escaperoom.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Escaperoom
+{
+    public partial class Groeplijst : Form
+    {
+        ConnectieDB database = new ConnectieDB();
+        
+        public List<Groep> lijstmetGroepen;
+        public string item;
+        public int id;
+        public Groep groep;
+
+
+        public Groeplijst()
+        {
+            InitializeComponent();
+        }
+        private void Groeplijst_Load(object sender, EventArgs e)
+        {
+            lijstmetGroepen = database.GetGroepenLijst();
+
+            listBox1.DataSource = new BindingSource(lijstmetGroepen, null);
+            listBox1.DisplayMember = "Naam";
+
+            
+            /*foreach (string groep in lijstmetGroepen)
+            {
+                listBox1.Items.Add(groep);
+            }*/
+            
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.item = listBox1.GetItemText(listBox1.SelectedItem);
+            MessageBox.Show(id.ToString());
+            //this.groep = new Groep(listBox1.SelectedItem.ToString());            
+            this.groep = new Groep(this.item);
+            //Groep name = new Groep(listBox1.SelectedItem);
+
+
+            // Hier functie komen voor het ophalen van de leerlingen.
+
+            this.Hide();            
+        }
+
+    }
+}
